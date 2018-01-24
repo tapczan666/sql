abc48d44384393cbdadb9c89e97786d8661c738d
Maciek W <Maciej.Wojcik4@networks.pl>
2018-01-24 12:19:04 +0100

    Importing from CSV files

---
+++ 04_sql.py
@@ -0,0 +1,18 @@
+# import from CSV
+
+# import the CSV library
+import csv
+
+import sqlite3
+
+with sqlite3.connect("new.db") as connection:
+	c = connection.cursor()
+
+	# open the CSV file and assign it to a variable
+	employees = csv.reader(open("employees.csv", "rU"))
+
+	# create a new table called employees
+	c.execute("CREATE TABLE employees(firstname TEXT, lastname TEXT)")
+
+	# insert data into table
+	c.executemany("INSERT INTO employees (firstname, lastname) VALUES (?, ?)", employees)
